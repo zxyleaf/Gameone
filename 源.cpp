22bac681bf29d12cce6b45bd32d1819376063eab
zxyleaf <211240065@smail.nju.edu.cn>
2022-03-06 17:04:04 +0800

    Revert "record the score"
    This reverts commit 0098d4d7d012662b159adfb679a0a8dcce5e8832.

--- 源.cpp
+++ 源.cpp
@@ -638,7 +638,6 @@ void deal_with_input(char ch)// ´¦Àí¼üÅÌÊäÈë
 			}
 			cout << endl;
 		}
-		cout << "Board:\n";
 		cout << "Player1 score : " << player1.score << endl;
 		cout << "Player2 score : " << player2.score << endl;
 	}
